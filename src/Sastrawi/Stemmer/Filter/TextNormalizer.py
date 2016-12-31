0c37dcb54561bd852ca4cb0064b6c4977f7ae511
Khairul Imam <ki65559@gmail.com>
2016-12-31 20:25:46 +0800

    change lower sequence

--- src/Sastrawi/Stemmer/Filter/TextNormalizer.py
+++ src/Sastrawi/Stemmer/Filter/TextNormalizer.py
@@ -1,7 +1,7 @@
 import re
 
 def normalize_text(text):
-    result = str.lower(text)
+    result = text.lower() #lower the text even unicode given
     result = re.sub(r'[^a-z0-9 -]', ' ', result, flags = re.IGNORECASE|re.MULTILINE)
     result = re.sub(r'( +)', ' ', result, flags = re.IGNORECASE|re.MULTILINE)
 

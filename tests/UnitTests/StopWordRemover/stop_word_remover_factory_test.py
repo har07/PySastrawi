0e40516fbeacf5add6dafc338e664ed530dcbb1a
widnyana <wid@widnyana.web.id>
2018-05-11 00:58:47 +0700

    add ability to manipulate stopwords

--- tests/UnitTests/StopWordRemover/stop_word_remover_factory_test.py
+++ tests/UnitTests/StopWordRemover/stop_word_remover_factory_test.py
@@ -1,6 +1,8 @@
 import unittest
-from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
+
 from Sastrawi.StopWordRemover.StopWordRemover import StopWordRemover
+from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
+
 
 class Test_StopWordRemoverFactoryTest(unittest.TestCase):
     def setUp(self):
@@ -9,6 +11,14 @@ class Test_StopWordRemoverFactoryTest(unittest.TestCase):
 
     def test_createStopWordRemover(self):
         self.assertIsInstance(self.factory.create_stop_word_remover(), StopWordRemover)
+
+    def test_appendStopwords(self):
+        new_stopwords = ['dengan', 'ia', 'bahwa', 'oleh']
+        self.factory.append_stop_words(new_stopwords)
+
+        for sw in new_stopwords:
+            self.assertIn(sw, self.factory.stopwords, "Failed to append {} to stopwords".format(sw))
+
 
 if __name__ == '__main__':
     unittest.main()

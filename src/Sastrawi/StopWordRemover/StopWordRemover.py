c7822a6783066c735076e69b92e0c308ead0d2a0
Rya Meyvriska <rya.meyvriska@mail.ugm.ac.id>
2018-09-19 19:25:46 +0700

    menambahkan minimal length, sehingga string dengan jumlah character kurang dari min_length akan di hapus juga

--- src/Sastrawi/StopWordRemover/StopWordRemover.py
+++ src/Sastrawi/StopWordRemover/StopWordRemover.py
@@ -7,10 +7,10 @@ class StopWordRemover(object):
     def get_dictionary(self):
         return self.dictionary
 
-    def remove(self, text):
+    def remove(self, text, min_length: int = 1):
         """Remove stop words."""
         words = text.split(' ')
-        stopped_words = [word for word in words if not self.dictionary.contains(word)]
+        stopped_words = [word for word in words if not self.dictionary.contains(word) and len(word) >= min_length]
 
         return ' '.join(stopped_words)
 

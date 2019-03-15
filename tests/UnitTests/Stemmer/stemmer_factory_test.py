15fe5d620b8c3731e8b307d05549eecd2ddc8dc9
Mufid Jamaluddin <mufid.jamaluddin@gmail.com>
2019-03-16 05:23:34 +0700

    Test Case

--- tests/UnitTests/Stemmer/stemmer_factory_test.py
+++ tests/UnitTests/Stemmer/stemmer_factory_test.py
@@ -24,34 +24,43 @@ class Test_StemmerFactoryTest(unittest.TestCase):
         if output != expected:
             raise AssertionError(str.format('output is {} instead of {}', output, expected))
 
+    # Test Waktu Stemming < 3 detik
+    # @author Mufid Jamaluddin
     def test_execution_time(self):
         start = time.time()
         sentence  = 'Rakyat memenuHi halaMan geDung DPR unTuk menyuarakan isi hatinya. Saat Itu, situasi sangat genting sekali. Terjadi kerusuhan yang mengiringi pergerakan mahasiswa yang memperjuangkan reformasi.'
 
         factory = StemmerFactory()
         stemmer = factory.create_stemmer()
-
         stemmer.stem(sentence)
 
         end = time.time()
 
         execution_time = end - start
-
-        # print(execution_time)
-
-        # test execution time < 3 seconds
         self.assertTrue(execution_time < 3)
 
     def test_getWordsFromFile(self):
         factory = StemmerFactory()
         factory.get_words_from_file()
 
+    # Test Stemming per Kata
+    # @author Mufid Jamaluddin
     def test_word_stemming(self):
         factory = StemmerFactory()
         stemmer = factory.create_stemmer()
         self.assertEqual('besar', stemmer.stem('terbesar'))
         self.assertEqual('abai', stemmer.stem('diabaikan'))
 
+    # Test Stemming dengan isDev=True (No Cache)
+    # @author Mufid Jamaluddin
+    def test_word_stemmingdev(self):
+        factory = StemmerFactory(isDev=True)
+        stemmer = factory.create_stemmer()
+        self.assertEqual('besar', stemmer.stem('terbesar'))
+        self.assertEqual('abai', stemmer.stem('diabaikan'))
+
+    # Test Stemming dengan list tokens
+    # @author Mufid Jamaluddin
     def test_tokens_stemming(self):
         factory = StemmerFactory()
         stemmer = factory.create_stemmer()

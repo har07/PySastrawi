f36c56d38f61a1a6a74f905b7a001cde519ace3a
kekavigi <keka.vigi@gmail.com>
2019-06-11 00:00:27 +0700

    refactor: mengurangi struktur data yang kompleks.
    Mengapa perubahan dilakukan:
    Dari penggunaannya, class ArrayDictionary dapat disederhanakan menjadi 
    tipe data set() yang memiliki kompleksitas searching setara dengan 
    dict(), namun lebih efisien memori karena hanya menyimpan nilai keys.

--- src/Sastrawi/Dictionary/DictionaryInterface.py
+++
@@ -1,5 +0,0 @@
-class DictionaryInterface(object):
-    """Interface definition of dictionary"""
-
-    def contains(self, word):
-        raise NotImplementedError('you must implement this method manually')
